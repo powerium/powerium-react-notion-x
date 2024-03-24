3bda6618f6ff69fc864dfbeb5385b2b615a01b67
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-03-24 14:57:36 -0400

    Change next/image to next/legacy/image
    For more details,
    visit https://nextjs.org/docs/messages/next-image-upgrade-to-13

--- next.config.js
+++ next.config.js
@@ -6,14 +6,39 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
   images: {
-    domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it'
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so'
+      },
+      {
+        protocol: 'https',
+        hostname: 'notion.so'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'abs.twimg.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.us-west-2.amazonaws.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'powerium.io'
+      },
+      {
+        protocol: 'https',
+        hostname: '**.powerium.io'
+      }
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
